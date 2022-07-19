52b27de18fea57b103d13cfd6b8972fbb390cc27
Taha Elmi <elmi.taha@gmail.com>
2022-07-20 01:23:39 +0430

    feat: ObjectFall method has been written but has bugs

--- Assets/Scripts/WhereIsMySoulManager.cs
+++ Assets/Scripts/WhereIsMySoulManager.cs
@@ -2,6 +2,7 @@ using System.Timers;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class WhereIsMySoulManager : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class WhereIsMySoulManager : MonoBehaviour
     public int soul;
     public string realTimeLabel;
     public int layerTime;
+    public GameObject starParent;
+    public GameObject soulGrabberParent;
+    public GameObject gameObject;
+    public Transform temp;
     
     private Timer realTimeTimer;
     private DateTime realDateTime;
@@ -18,15 +23,41 @@ public class WhereIsMySoulManager : MonoBehaviour
     private static string deathSceneName = "death"; 
     private static string awakeningSceneName = "awakening"; 
     private Timer layerTimeTimer;
+    private Transform[] stars;
+    private GameObject[] starsObjs;
+    private Transform[] soulGrabbers;
+    private int currentStar;
+    private int currentSoulGrabber;
+    private Timer objectFallTimer;
+
+    private GameObject[] starChildren;
 
     // Start is called before the first frame update
     void Start()
     {
         layer = 1;
         soul = 0;
+        stars = starParent.transform.GetComponentsInChildren<Transform>(true);
+        currentStar = 1;
+        starsObjs = new GameObject[stars.Length];
+        for (int i = 0; i < stars.Length; i++)
+        {
+            starsObjs[i] = stars[i].gameObject;
+        }
+        soulGrabbers = soulGrabberParent.GetComponentsInChildren<Transform>(true);
+        currentSoulGrabber = 1;
+
         SetTimers();
         realTimeTimer.Start();
         layerTimeTimer.Start();
+        objectFallTimer.Start();
+    }
+
+    private void OnDestroy()
+    {
+        realTimeTimer.Stop();
+        layerTimeTimer.Stop();
+        objectFallTimer.Stop();
     }
 
     private void RealTimeTick(object sender, EventArgs e)
@@ -48,6 +79,51 @@ public class WhereIsMySoulManager : MonoBehaviour
         }
     }
 
+
+    private GameObject getGameObjectFromTransform(Transform t)
+    {
+        return t.gameObject;
+    }
+    
+    private void ObjectFall(object sender, EventArgs e)
+    {
+        if (soul == 0 || Random.value < 0.5)
+        {
+            gameObject = starsObjs[currentStar];
+            
+            print("soul");
+            print("the index is: " + currentStar);
+            print("len stars is: " + stars.Length);
+
+            currentStar++;
+            if (currentStar >= stars.Length)
+            {
+                currentStar = 1;
+            }
+
+        }
+        else
+        {
+            print("grabber");
+            gameObject = soulGrabbers[currentSoulGrabber].gameObject;
+
+            currentSoulGrabber++;
+            if (currentSoulGrabber >= soulGrabbers.Length)
+            {
+                currentSoulGrabber = 1;
+            }
+        }
+
+        // float x = Random.Range(-7, 7);
+        float x = 5.0f;
+        print("before pos set");
+        gameObject.transform.position.Set(x, gameObject.transform.position.y, gameObject.transform.position.z);
+        print("after pos set");
+        
+        gameObject.SetActive(true);
+        print("activated");
+    }
+
     private void downLayer()
     {
         layer += 1;
@@ -63,6 +139,7 @@ public class WhereIsMySoulManager : MonoBehaviour
         };
         realTimeTimer.Elapsed += RealTimeTick;
         realTimeTimer.Enabled = true;
+        
         layerTime = layerTimeInitialValue;
         layerTimeTimer = new Timer
         {
@@ -70,6 +147,13 @@ public class WhereIsMySoulManager : MonoBehaviour
         };
         layerTimeTimer.Elapsed += LayerTimeTick;
         layerTimeTimer.Enabled = true;
+        
+        objectFallTimer = new Timer
+        {
+            Interval = 2000
+        };
+        objectFallTimer.Elapsed += ObjectFall;
+        objectFallTimer.Enabled = true;
     }
 
     // Update is called once per frame
--- Assets/Scripts/getFallingObjects.cs
+++ Assets/Scripts/getFallingObjects.cs
@@ -7,21 +7,7 @@ public class getFallingObjects : MonoBehaviour
 {
 
     public WhereIsMySoulManager manager;
-    public GameObject starsParent;
-    public GameObject soulGrabbersParent;
-    // -9 to 7.5
 
-    private Transform[] stars;
-    private Transform[] soulGrabbers;
-
-    private void Start()
-    {
-        stars = starsParent.GetComponentsInChildren<Transform>();
-        soulGrabbers = soulGrabbersParent.GetComponentsInChildren<Transform>();
-        print("strs: " + stars.Length);
-        print("grbs: " + soulGrabbers.Length);
-    }
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name.StartsWith("star"))
@@ -36,8 +22,5 @@ public class getFallingObjects : MonoBehaviour
         other.gameObject.SetActive(false);
         
         other.gameObject.transform.position = new Vector3(0, 3.5f, 0);
-        
-        other.gameObject.SetActive(true);
-
     }
 }
